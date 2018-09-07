108681abb6fca45e271d9106966aeed3b4c3470a
Jasmine Allen <synee00@gmail.com>
2018-09-07 15:09:08 -0400

    Google Natural Language API working version

---
+++ src/main/java/co/grandcircus/BeBetter/BeBetterController.java
@@ -0,0 +1,59 @@
+package co.grandcircus.BeBetter;
+
+import com.google.cloud.language.v1.LanguageServiceClient;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.Document.Type;
+import com.google.cloud.language.v1.Sentiment;
+
+@Controller
+public class BeBetterController {
+	
+	
+	@RequestMapping("/")
+	public ModelAndView index()
+	{
+		ModelAndView mav = new ModelAndView("index");
+		
+		
+		
+		
+		// Instantiates a client
+		LanguageServiceClient language;
+		try {
+			language = LanguageServiceClient.create();
+			
+			// The text to analyze
+			String[] texts = {"I love this!", "I hate this!"};
+			for (String text : texts)
+			{
+			  Document doc = Document.newBuilder().setContent(text).setType(Type.PLAIN_TEXT).build();
+			  // Detects the sentiment of the text
+			  Sentiment sentiment = language.analyzeSentiment(doc).getDocumentSentiment();
+
+			  System.out.printf("Text: \"%s\"%n", text);
+			  System.out.printf(
+			      "Sentiment: score = %s, magnitude = %s%n",
+			      sentiment.getScore(), sentiment.getMagnitude());
+			  mav.addObject("score", sentiment.getScore());
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+		
+		
+		return mav;
+		
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Be Better</title>
+</head>
+<body>
+	<h1> Welcome!! </h1>
+	
+	<p>Score: ${score} </p>
+</body>
+</html>
